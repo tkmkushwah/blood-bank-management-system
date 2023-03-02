9c41d827c5e740bdaea0926acd7645682430fba2
saurabh sharma <saurabh08b@gmail.com>
2023-03-02 22:35:44 +0530

    small change

--- client/src/components/Admin/AdminDashborad.jsx
+++ client/src/components/Admin/AdminDashborad.jsx
@@ -3,7 +3,7 @@ import Layout from '../Layout/Layout.jsx'
 const AdminDashborad = () => {
   return (
     <Layout>
-      <h2>AdminDashborad</h2>
+      <h2>AdminDashborad saurabh</h2>
     </Layout>
   );
 }

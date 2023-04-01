4a076ca831519cd984f62f0eeb06072b32294d3d
saurabh sharma <saurabh08b@gmail.com>
2023-04-01 15:08:18 +0530

    home gallary

--- client/src/components/Home/GalleryHome.jsx
+++ client/src/components/Home/GalleryHome.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import img1 from '../../assets/img1.jpg'
+import HomeGallary1 from '../../assets/HomeGallary1.png'
+import HomeGallary2 from '../../assets/HomeGallary2.jpg'
+import HomeGallary3 from '../../assets/HomeGallary3.jpg'
 import '../../styles/gallery.scss'
 const GalleryHome = () => {
   return (
@@ -8,9 +10,9 @@ const GalleryHome = () => {
         <h1 style={{ textAlign: "center",paddingTop:'30px' }}>Gallery</h1>
       </div>
       <div className="images">
-        <img src={img1} alt="ojioajpf" />
-        <img src={img1} alt="jadio" />
-        <img src={img1} alt="ajf'oaj" />
+        <img src={HomeGallary1} alt="ojioajpf" />
+        <img src={HomeGallary2} alt="jadio" />
+        <img src={HomeGallary3} alt="ajf'oaj" />
       </div>
     </section>
   );

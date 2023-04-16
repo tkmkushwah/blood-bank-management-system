cab40f11989069148bc215300a83cd323e67f5b0
riteshshami <96076485+riteshshami@users.noreply.github.com>
2023-04-16 16:07:43 +0530

    Updated Learn section

--- client/src/components/About/Eligibility.jsx
+++ client/src/components/About/Eligibility.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Layout from '../Layout/Layout'
-import '../../styles/bloodtypes.scss'
+import React from "react";
+import Layout from "../Layout/Layout";
+import "../../styles/bloodtypes.scss";
 
 const Eligibility = () => {
   return (
@@ -12,25 +12,62 @@ const Eligibility = () => {
             <div className="eligibility-box">
               <h5>Whole Blood Donation</h5>
               <ul>
-                <li>Donation frequency: Every 56 days, up to 6 times a year</li>
-                <li>You must be in good health and feeling well</li>
-                <li>You must be at least 16 years old in most states</li>
-                <li>You must weigh at least 110 lbs</li>
+                <li>Age: Donors should be between 18 to 65 years of age.</li>
+                <li>Weight: Donors should weigh at least 50 kg.</li>
+                <li>
+                  Hemoglobin level: Hemoglobin level should be 12.5 g/dL or
+                  above.
+                </li>
+                <li>
+                  Health: Donors should be in good health and should not have
+                  any acute or chronic illnesses.
+                </li>
+                <li>
+                  Medications: Donors should not be taking any medications for
+                  chronic illnesses such as diabetes, hypertension, or cancer.
+                </li>
+                <li>
+                  {" "}
+                  Lifestyle: Donors should not have engaged in high-risk
+                  behaviors such as drug use, unprotected sex, or multiple
+                  sexual partners.
+                </li>
+                <li>
+                  Pregnancy and breastfeeding: Women who are pregnant or
+                  breastfeeding should not donate blood.
+                </li>
               </ul>
             </div>
 
             <div className="eligibility-box">
               <h5>Power Red Donation</h5>
               <ul>
-                <li>Donation frequency: Every 112 days, upto 3 times/year</li>
-                <li>You must be in good health and feeling well</li>
+                <li>Age: Donors should be between 18 to 60 years of age.</li>
+                <li>Weight: Donors should weigh at least 55 kg.</li>
                 <li>
-                  <b>Male donors</b> must be at least 17 years old in most
-                  states, at least 5'11' tall and weight at least 130 lbs.
+                  Hemoglobin level: Hemoglobin level should be 12.5 g/dL or
+                  above for women and 13.0 g/dL or above for men.
                 </li>
                 <li>
-                  <b>Female donors</b> must be at least 19 years old, at least
-                  5'5" tall and weigh at least 150 lbs
+                  Blood type: Donors should have A negative, B negative, or O
+                  negative blood type.
+                </li>
+                <li>
+                  Health: Donors should be in good health and should not have
+                  any acute or chronic illnesses.
+                </li>
+                <li>
+                  Medications: Donors should not be taking any medications for
+                  chronic illnesses such as diabetes, hypertension, or cancer.
+                </li>
+                <li>
+                  Lifestyle: Donors should not have engaged in high-risk
+                  behaviors such as drug use, unprotected sex, or multiple
+                  sexual partners.
+                </li>
+                <li>
+                  Pregnancy and breastfeeding: Women who are pregnant or
+                  breastfeeding should not donate blood.
                 </li>
               </ul>
             </div>
@@ -38,28 +75,53 @@ const Eligibility = () => {
             <div className="eligibility-box">
               <h5>Platelet Donation</h5>
               <ul>
-                <li>Donation frequency: Every 7 days, upto 24 times/year</li>
-                <li>You must be in good health and feelinf well</li>
-                <li>You must be at least 17 years old in most states</li>
-                <li>You must weigh at least 110 lbs</li>
+                <li>Age: Donors should be between 18 to 65 years of age.</li>
+                <li>Weight: Donors should weigh at least 55 kg.</li>
+                <li>
+                  Hemoglobin level: Hemoglobin level should be 12.5 g/dL or
+                  above for women and 13.0 g/dL or above for men.
+                </li>
+                <li>
+                  Blood type: Donors should have A positive, A negative, B
+                  positive, B negative, AB positive, or O positive blood type.
+                </li>
+                <li>
+                  Health: Donors should be in good health and should not have
+                  any acute or chronic illnesses.
+                </li>
+                <li>
+                  Medications: Donors should not be taking any medications for
+                  chronic illnesses such as diabetes, hypertension, or cancer.
+                </li>
+                <li>
+                  Lifestyle: Donors should not have engaged in high-risk
+                  behaviors such as drug use, unprotected sex, or multiple
+                  sexual partners.
+                </li>
+                <li>
+                  Platelet count: Donors should have a platelet count of 1.5
+                  lakh per microliter or above.
+                </li>
               </ul>
             </div>
 
             <div className="eligibility-box">
               <h5>AB Elite Plasma Donation</h5>
               <ul>
-                <li>Donation frequency: Every 28 days, up to 13 times/year</li>
-                <li>You must have type AB blood</li>
-                <li>You must be in good health and feeling well</li>
-                <li>You must be at least 17 years old</li>
-                <li>You must weigh at least 110 lbs</li>
+                <li>Age: Donors should be between 18 to 65 years of age.</li>
+                <li>Weight: Donors should weigh at least 50 kg.</li>
+                <li>Hemoglobin level: Hemoglobin level should be 12.5 g/dL or above for women and 13.0 g/dL or above for men.</li> 
+                <li>Blood type: Donors should have AB positive or AB negative blood type. Health: Donors should be in good health and should not have any acute or chronic illnesses.</li> 
+                <li>Medications: Donors should not be taking any medications for chronic illnesses such as diabetes, hypertension, or cancer.</li>
+                <li>Lifestyle: Donors should not have engaged in high-risk behaviors such as drug use, unprotected sex, or multiple sexual partners.</li>
               </ul>
             </div>
           </div>
         </div>
       </section>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </Layout>
   );
-}
+};
 
-export default Eligibility
+export default Eligibility;

ff838398e29938c3eac554dcce7665551525665d
Tikam Singh <tikamsingh1901900@gmail.com>
2023-05-09 19:04:41 +0530

    responsive homepage

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,60 +1,44 @@
 import React from "react";
 import AboutHome from "./AboutHome";
-// import { useAuth } from "../../context/auth.js";
 import Layout from "../Layout/Layout";
 import { NavLink } from "react-router-dom";
 import GalleryHome from "./GalleryHome";
 import ContactHome from "./ContactHome";
 import eligibilityIcon from "../../assets/eligible-icon.png";
+import '../../styles/app.scss'
 
 const Home = () => {
-  // const [auth, setAuth] = useAuth();
   return (
     <Layout>
-      <section className="home">
-        <div>
-          <h1 style={{ paddingTop: "20px" }}>Blood Donation</h1>
-          <marquee>This project is under development</marquee>
-          {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
-          <p
-            style={{
-              fontSize: "1.5rem",
-              textTransform: "uppercase",
-              fontFamily: "Roboto",
-              margin: " 2rem 0",
-            }}
-          >
-            we are here to provide you best facilities
-          </p>
-          <br />
+      <div className="Home" style={{ padding: "0%" }}>
+        {/* <h1 style={{ paddingTop: "10px" }}>Blood Donation</h1> */}
 
-          <div className="eligibility-btn-container">
-            <NavLink to="/eligibility">
-              <img src={eligibilityIcon} alt="icon" />
-            </NavLink>
+        <marquee style={{ color: "white" }}>
+          This project is under development
+        </marquee>
+        {/* <p>we are here to provide you best facilities</p> */}
+<div className="container">
+   <div className="eligibility-btn-ctnr" style={{ padding: "0%" }}>
+          <NavLink to="/eligibility">
+            <img src={eligibilityIcon} alt="icon" />
+          </NavLink>
 
-            <button>
-              <NavLink to="/bloodbanklogin">Are you BloodBank?</NavLink>
-            </button>
-          </div>
-
-          <div
-            className="login-btn-container"
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginTop: "50px",
-            }}
-          >
-            <button>
-              <NavLink to="/login">Login</NavLink>
-            </button>
-            <button>
-              <NavLink to="/register">Register</NavLink>
-            </button>
-          </div>
+          <button>
+            <NavLink to="/bloodbanklogin">Are you BloodBank?</NavLink>
+          </button>
         </div>
-      </section>
+
+        <div className="login-btn-cntr" style={{ padding: "0%"}}>
+          <button>
+            <NavLink to="/login">Login</NavLink>
+          </button>
+          <button>
+            <NavLink to="/register">Register</NavLink>
+          </button>
+        </div>
+</div>
+     
+      </div>
       <AboutHome />
       <GalleryHome />
       <ContactHome />
